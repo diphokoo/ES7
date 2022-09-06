7f3272c232cfb9209a62612d2f8f42d49946f2b1
diphokoo <diphokoo@outlook.com>
2022-09-06 07:23:53 +0200

    for loop

---
+++ for of loop.js
@@ -0,0 +1,14 @@
+let incomes = [62000, 67000, 75000];
+let total = 0;
+
+for (const income of incomes) {
+    total += income;
+}
+
+console.log(total);
+
+let fullName = "Obi Coding King";
+
+for (const char of fullName) {
+    console.log(char);
+}
